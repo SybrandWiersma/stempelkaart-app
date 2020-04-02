da68350a5d783b7bf188b8b99e21e1a186a3e6a5
Myrdin <myrdin.willemse@student.nhlstenden.com>
2020-04-02 22:36:44 +0200

    update

--- config.php
+++ config.php
@@ -1,31 +1,15 @@
 ﻿<?php
-$host		=	"localhost";					
-$user		=	"root";				            
-$pass		=	"";					           
-$tablename	=	"stempelkaartapp";				
-
-mysql_connect("$host","$user","$pass") && mysql_select_db("$tablename");
-
+<?php
 session_start();
+$host = "localhost"; 
+$user = "root"; 
+$password = ""; 
+$dbname = "stempelkaartapp"; 
 
-if(isset($_SESSION['login'])){
-    $dbresondernemer = mysql_query("SELECT * FROM `ondernemers` WHERE `gebr_naam`='{$_SESSION['login']}'");
-    $dbresklant = mysql_query("SELECT * FROM `klanten` WHERE `gebr_naam`='{$_SESSION['login']}'");
-    $data = mysql_fetch_object($dbresondernemer);
-    $data2 = mysql_fetch_object($dbresklant);
-    $_COOKIE['login'] = $_SESSION['login']
-    $login = $_COOKIE['login'];
+$con = mysqli_connect($host, $user, $password,$dbname);
+// Checken of er verbinding is
+if (!$con) {
+ die("Verbinding mislukt: " . mysqli_connect_error());
 
-} else {
-    unset($_SESSION['login']);
-}
-
-function check_login() {
-    if(isset($_SESSION['login'])){
-    return true;
-    } else { 
-    return false;
-    }
-}
 
 ?>
