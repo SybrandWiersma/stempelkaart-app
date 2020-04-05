4ed12fb658c547e2ccb33215ace85f766aa1a6d5
Myrdin <myrdin.willemse@student.nhlstenden.com>
2020-04-05 16:43:24 +0200

    Kleine aanpassingen en landingpagina voor ondernemers + uitloggen
    Volledige mogelijkheid nu om een account aan te maken en vervolgens in te loggen op je account en daadwerkelijk in een omgeving terecht komen

---
+++ config.php
@@ -0,0 +1,15 @@
+﻿
+<?php
+session_start();
+$host = "localhost"; 
+$user = "root"; 
+$password = ""; 
+$dbname = "stempelkaartapp"; 
+
+$con = mysqli_connect($host, $user, $password,$dbname);
+// Checken of er verbinding is
+if (!$con) {
+ die("Verbinding mislukt: " . mysqli_connect_error());
+ }
+
+?>
--- landing_ondernemer.php
+++ landing_ondernemer.php
@@ -1,7 +1,11 @@
 <?php
 include("config.php");
 
-// Uitloggen
+// Check of gebruiker ingelogd is of niet
+if(!isset($_SESSION['gebruikersnaam'])){
+    header('Location: index.php');
+}
+// Uitloggen (eerste check of er een 'x' in de browser meegegeven wordt, zoja als dat uitloggen is word je uitgelogd)
 if(isset($_GET['x'])){
     if($_GET['x'] == "uitloggen"){
     session_destroy();
@@ -13,7 +17,7 @@ if(isset($_GET['x'])){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Een digitaal welkom</title>
+    <title>Ondernemerspagina</title>
     <link rel="stylesheet" href="style.css" type="text/css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 </head>
@@ -36,16 +40,21 @@ if(!isset($_SESSION['gebruikersnaam'])){
     <div>
         <h1><a href="">StempelkaartApp</a></h1> 
         <a href=""><i class="fas fa-user-circle"></i>Profiel</a>
-        <a href="index.php?x=uitloggen"><i class="fas fa-sign-out-alt"></i>Uitloggen</a>
+        <a href="landing_ondernemer.php?x=uitloggen"><i class="fas fa-sign-out-alt"></i>Uitloggen</a>
     </div>
 <?php
  }
  ?>
 </nav>
 <div class="wrapper">
-    <h1>Welkom!</h1>
-
-
+    <h1>Ondernemerspagina</h1>
+    <form action="">
+       <input type="submit" name="send" value="Taak 1 ">
+       <input type="submit" name="send" value="Taak 2">
+       <input type="submit" name="send" value="Taak 3">
+       <input type="submit" name="send" value="Taak 4">
+       <input type="submit" name="send" value="Taak 5">
+       </form>
 
         
         
