481528098d57bcd80c64fcea55191dbec1c168a9
SybrandWiersma <43571153+SybrandWiersma@users.noreply.github.com>
2020-04-05 13:53:38 +0200

    Update config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+$host = "localhost"; 
+$user = "root"; 
+$password = ""; 
+$dbname = "stempelkaartapp"; 
+
+$con = mysqli_connect($host, $user, $password,$dbname);
+// Checken of er verbinding is
+if (!$con) {
+ die("Verbinding mislukt: " . mysqli_connect_error());
+ }
+
+?>
