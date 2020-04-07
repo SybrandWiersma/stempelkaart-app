a40a273e2ac2184d01e1f478788bba663be9267f
Unknown <frederic.de.haan@student.nhlstenden.com>
2020-04-07 23:09:04 +0200

    Beloning + Klantstempelkaartoverzicht, CSS en stempelkaartinfo gewijzigd
    Beloning kan bekenen worden en klant heeft nu een overzicht van stempelkaarten
    CSS verder gewijzigd om zo responsive mogelijk te maken
    Stempelkaartinfo buttons hebben nu werkende href

--- beloning.php
+++ beloning.php
@@ -6,7 +6,7 @@ include("config.php")
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Stempelkaart Info</title>
+    <title>Beloning</title>
     <link rel="stylesheet" href="style.css" type="text/css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 </head>
@@ -18,21 +18,17 @@ include("config.php")
         <a href=""><i class="fas fa-sign-out-alt"></i>Uitloggen</a>
     </div>
 </nav>
-<div class="wrapperStempelkaartInfo">
+<div class="wrapper">
     <h1>Snackbar Vette Hap</h1>
-    <h2 style="margin-left: -60%">Stempels: </h2>
-    <div class="StempelsInfo">
-
-    </div>
-    <button onclick="window.location.href='qr-code.html'" style="width: 40%">QR-Code</button><br>
-    <button onclick="window.location.href='beloning.html'" style="width: 40%">Beloning Bekijken</button><br>
-    <button onclick="goBack()" style="width: 40%; margin-bottom: 5%"><i class="fas fa-chevron-left"></i> Terug naar Kaarten</button> <br>
+    <h2>Beloning</h2>
+    <h4>Bij verzilvering van uw stempels krijgt u op uw volgende bestelling 10 euro korting</h4>
+    <button onclick="goBack()" style="width: 40%; margin-bottom: 5%; margin-left: 30%"><i class="fas fa-chevron-left"></i> Terug</button>
     <script>
         function goBack() {
             window.history.back();
         }
     </script>
-     <button style="width: 25%; background-color: red; margin-bottom: 5%">Verwijderen</button>
+
 </div>
 </body>
 </html>
