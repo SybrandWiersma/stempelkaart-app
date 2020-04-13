b23a8d514fe994746391474425ad1cff456aa1e2
Myrdin <myrdin.willemse@student.nhlstenden.com>
2020-04-13 21:44:49 +0200

    Klant kaart weergeven + QR

---
+++ klant_kaartweergeven.php
@@ -0,0 +1,183 @@
+<?php
+include("config.php");
+
+// Check of gebruiker ingelogd is of niet
+if(!isset($_SESSION['gebruikersnaam'])){
+    header('Location: index.php');
+}
+// Uitloggen (eerste check of er een 'x' in de browser meegegeven wordt, zoja als dat uitloggen is word je uitgelogd)
+if(isset($_GET['x'])){
+    if($_GET['x'] == "uitloggen"){
+    session_destroy();
+    header('Location: index.php');
+    }
+}
+
+
+//om fraude te voorkomen eerst een check of er een p en een o meegegeven worden
+if(!isset($_GET['p']) && !isset($_GET['o'])){
+        header('Location: 404.php');
+} else {
+
+    //query om klant gegevens uit de database op te halen
+    $sql_id = "SELECT  * FROM `klanten` WHERE `naam_klant`='".$_SESSION['gebruikersnaam']."'";
+    $sql_query_id = mysqli_query($con,$sql_id);
+    $result_id = mysqli_fetch_object($sql_query_id);
+
+   //query om gegevens stempelkaart uit de database op te halen
+    $sql_stemp = "SELECT  * FROM `stempelkaarten` WHERE `stempelkaart_id`='".$_GET['p']."'";
+    $sql_query_stemp = mysqli_query($con,$sql_stemp);
+    $result_stemp = mysqli_fetch_object($sql_query_stemp);
+
+    //query om ondernemers gegevens uit de database op te halen voor afbeeldingen
+    $sql_ond = "SELECT  * FROM `ondernemers` WHERE `ondernemer_id`='".$result_stemp->ondernemer_id."'";
+    $sql_query_ond = mysqli_query($con,$sql_ond);
+    $result_ond = mysqli_fetch_object($sql_query_ond);
+
+    //query om aantal stempels  uit de database op te halen 
+    $sql_aant = "SELECT  * FROM `stempelkaart_klant` WHERE `stempelkaart_id`='".$_GET['p']."'";
+    $sql_query_aant = mysqli_query($con,$sql_aant);
+    $result_aant = mysqli_fetch_object($sql_query_aant);
+
+    //als de meegegeven o niet overeenkomt met het ondernemers id van de ingelogde persoon kom je op 404
+    if($_GET['o'] != $result_id->klant_id){
+                header('Location: 404.php');
+	} else {
+    
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Kaart bekijken</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+
+</head>
+<body>
+<nav class="navtop">
+<?php
+if(!isset($_SESSION['gebruikersnaam'])){
+
+?>
+
+    <div>
+       <h1><a href="index.php">StempelkaartApp</a></h1>
+        <a href="ondernemer_registeren.php"><i class="fas fa-user-circle"></i>Registreren als ondernemer</a>
+        <a href="klant_registratie.php"><i class="fas fa-user-circle"></i>Registreren als klant</a>
+        <a href="loginpagina.php"><i class="fas fa-sign-out-alt"></i>Inloggen</a>
+    </div>
+<?php
+} else {
+?>
+    <div>
+        <h1><a href="ondernemer_landing.php">StempelkaartApp</a></h1>
+        <a href="ondernemer_gegevensbekijken.php"><i class="fas fa-user-circle"></i>Profiel</a>
+        <a href="ondernemer_landing.php?x=uitloggen"><i class="fas fa-sign-out-alt"></i>Uitloggen</a>
+    </div>
+<?php
+ }
+ ?>
+</nav>
+<div class="wrapper" style="overflow-x:auto;">
+
+    <h1><?php echo $result_stemp->beloning_label; ?></h1>
+    <center>
+  
+    <img src="<?php print $result_ond->logo;?>" width="250px" height="170px">
+    <table class="noBorder">
+    <tr>
+    <?php 
+    
+        //for loop om vakken te weergeven MET stempel erin
+     for($i = 1; $i<=$result_aant->aant_stemps; $i+=1){
+
+     
+            if($i == 6 || $i == 12 || $i  == 18 || $i  == 24 || $i  == 25){
+             ?>
+                 <td width="15%" >
+                 <img src="<?php print $result_ond->stemp_afb;?>" width="50px" heigth="50px">
+                 </td>
+            </tr>
+            <tr>
+            <?php 
+            } else {
+            ?>
+
+    <td width="15%" >
+    <img src="<?php print $result_ond->stemp_afb;?>" width="50px" heigth="50px">
+    </td>
+   
+    <?php
+    }
+    }       
+ ?>
+ 
+ <?php
+
+        //for loop om vakken te weergeven ZONDER stempel erin
+     for($x = 1; $x<=$result_stemp->beloning_aantstemps - $result_aant->aant_stemps; $x+=1){
+
+     
+            if($x +  $result_aant->aant_stemps == 6 || $x +  $result_aant->aant_stemps  == 12 || $x +  $result_aant->aant_stemps   == 18 || $x +  $result_aant->aant_stemps   == 24 || $x +  $result_aant->aant_stemps   == 25){
+             ?>
+                 <td width="15%" height="70px">
+                 </td>
+            </tr>
+            <tr>
+            <?php 
+            } else {
+            ?>
+
+    <td width="15%" height="70px" >
+    </td>
+   
+    <?php
+    }
+    }       
+ ?>
+
+ 
+     </tr>
+
+  
+ 
+    </table>
+    <?php
+        if(!isset($_GET['z'])){
+        ?>
+    <button onclick="location.href='klant_kaartweergeven.php?p=<?php print $_GET['p'];?>&o=<?php print $_GET['o'];?>&z=toon';" id="btn_under"> Toon QR</button>
+    
+    <?php
+    }
+    if(isset($_GET['z'])){
+    if($_GET['z'] == "toon"){
+    ?>
+    <button onclick="location.href='klant_kaartweergeven.php?p=<?php print $_GET['p'];?>&o=<?php print $_GET['o'];?>';" id="btn_under"> Verberg QR</button>
+    <img src="https://chart.googleapis.com/chart?cht=qr&chl=<?$result_id->klant_id?>&choe=UTF-8&chs=400x400">
+    <?php
+    }
+    }
+    ?>
+    </center>
+           <center>
+    <button onclick="location.href='klant_stempelkaartoverzicht.php';" id="btn_under"><i class="fas fa-chevron-left"></i> Terug</button>
+       </center>
+
+         <h1></h1>
+    </div
+        
+        
+        
+
+
+
+</div>
+</body>
+</html>
+
+<?php
+	}
+}
+?>
--- klant_stempelkaartoverzicht.php
+++ klant_stempelkaartoverzicht.php
@@ -12,6 +12,12 @@ if(isset($_GET['x'])){
     header('Location: index.php');
     }
 }
+
+
+
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -40,7 +46,7 @@ if(!isset($_SESSION['gebruikersnaam'])){
     <div>
         <h1><a href="">StempelkaartApp</a></h1> 
         <a href=""><i class="fas fa-user-circle"></i>Profiel</a>
-        <a href="ondernemer_landing.php?x=uitloggen"><i class="fas fa-sign-out-alt"></i>Uitloggen</a>
+        <a href="klant_stempelkaartoverzicht.php?x=uitloggen"><i class="fas fa-sign-out-alt"></i>Uitloggen</a>
     </div>
 <?php
  }
@@ -50,38 +56,61 @@ if(!isset($_SESSION['gebruikersnaam'])){
     <h1>Uw Stempelkaarten</h1>
     <div class="StempelkaartOverzicht_div">
         <ul style="list-style-type:none;">
-            <li class="Stempelkaart">
+        <?php 
+
+            $message = "";
+           //query om gegevens klant uit de database op te halen
+             $sql_klant = "SELECT  * FROM `klanten` WHERE `naam_klant`='".$_SESSION['gebruikersnaam']."'";
+             $sql_query_klant = mysqli_query($con,$sql_klant);
+             $result_klant = mysqli_fetch_object($sql_query_klant);
+
+             //query om aantal gekoppelde kaarten te tellen
+             $stmt = $con->prepare("SELECT * FROM stempelkaart_klant WHERE klant_id = ?");
+             $stmt->bind_param("s", $result_klant->klant_id);
+             $stmt->execute();
+             $result = $stmt->get_result();
+             $stmt->close();
+
+             if($result->num_rows < 1){
+             $message = "<h2>U bent nog niet gekoppeld aan een stempelkaart, neem contact op met het bedrijf!</h2>";
+             }
+
+            //query om gegevens persoonlijke stempelkaart uit de database op te halen
+             $sql_pers = "SELECT  * FROM `stempelkaart_klant` WHERE `klant_id`='".$result_klant->klant_id."'";
+             $sql_query_pers = mysqli_query($con,$sql_pers);
+             while($result_pers = mysqli_fetch_object($sql_query_pers)){
+
+
+
+            //query om gegevens stempelkaarten waaraan klant is gekoppeld uit de database op te halen
+             $sql_kaart = "SELECT  * FROM `stempelkaarten` WHERE `stempelkaart_id`='".$result_pers->stempelkaart_id."'";
+             $sql_query_kaart = mysqli_query($con,$sql_kaart);
+             while($result_kaart = mysqli_fetch_object($sql_query_kaart)){
+             ?>
+
+
+            <a href='klant_kaartweergeven.php?p=<?php print $result_pers->stempelkaart_id;?>&o=<?php print $result_klant->klant_id;?>'><li class="Stempelkaart">
+
+
                 <div id="ond_naam">
-                    <h2>Snackbar Vette Hap</h2>
+                    <h2><?php echo $result_kaart->beloning_label;?></h2>
                 </div>
                 <div id="aant_stemp">
-                    <h2> 3/8</h2>
+                    <h2> <?php echo $result_pers->aant_stemps;?>/<?php echo $result_kaart->beloning_aantstemps;?></h2>
                 </div>
-            </li>
-            <li class="Stempelkaart">
-                <div id="ond_naam">
-                    <h2>Kapper Knipschaar</h2>
-                </div>
-                <div id="aant_stemp">
-                    <h2> 4/12</h2>
-                </div>
-            </li>
-            <li class="Stempelkaart">
-                <div id="ond_naam">
-                    <h2>Schoonheidssalon Marije</h2>
-                </div>
-                <div id="aant_stemp">
-                    <h2> 1/6</h2>
-                </div>
-            </li>
+            </li></a>
+
+            <?php
+            }
+            }
+            if(!empty($message)){
+            echo $message;  
+            }
+            ?>
+  
         </ul>
     </div>
     <div style="border-bottom: 1px solid #dee0e4"></div>
-    <button onclick="window.location.href='#'">Voeg een stempelkaart toe</button>
-    <button onclick="goBack()" style="width: 40%; margin-bottom: 5%"><i class="fas fa-chevron-left"></i> Terug</button> <br>
-    <script>
-        function goBack() {
-            window.history.back();
-        }
-    </script>
+
+
 </div>
