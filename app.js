7fa7e04bec1b820de22f396fea735453acb1e872
Unknown <frederic.de.haan@student.nhlstenden.com>
2020-04-13 16:11:26 +0200

    Gescande kaart styling
    Border voor gescande kaart onder de preview

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ let scanner = new Instascan.Scanner({ video: document.getElementById('preview') 
 scanner.addListener('scan', function (content, image) {
     console.log(content);
     let node = document.createElement("LI");
+    node.style.listStyle = "none";
+    node.style.border = "1px solid black";
+    node.style.borderRadius = "5px";
+    node.style.marginRight = "10%";
+    node.style.marginTop = "10%";
+    node.style.padding = "4%";
     let v1 = document.createTextNode(content);
     node.appendChild(v1);
     document.getElementById("scans").appendChild(node);
