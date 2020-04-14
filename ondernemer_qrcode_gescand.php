bf0183d990690e8e0eedf649f6d38b80c7c59b2b
Sybrand Wiersma <sybrandw@live.nl>
2020-04-14 13:13:38 +0200

    kleine save

--- ondernemer_qrcode_gescand.php
+++ ondernemer_qrcode_gescand.php
@@ -1,5 +1,37 @@
 <?php
-include("config.php")
+include("config.php");
+
+// Check of gebruiker ingelogd is of niet
+if (!isset($_SESSION['gebruikersnaam'])) {
+    header('Location: index.php');
+}
+// Uitloggen (eerste check of er een 'x' in de browser meegegeven wordt, zoja als dat uitloggen is word je uitgelogd)
+if (isset($_GET['x'])) {
+    if ($_GET['x'] == "uitloggen") {
+        session_destroy();
+        header('Location: index.php');
+    }
+}
+
+$query_ondernemingID = "SELECT ondernemer_id FROM ondernemers WHERE gebr_naam = '".$_SESSION['gebruikersnaam']."';";
+$result_ondernemingID = mysqli_query($con, $query_ondernemingID);
+$row_ondernemingID = mysqli_fetch_array($result_ondernemingID);
+
+$ondernemer_id = $row_ondernemingID['ondernemer_id'];
+$klant_id = $_GET['klantid'];
+$kaart_id = $_GET['kaartid'];
+
+if(!isset($klant_id) && !isset($kaart_id)){
+    header('Location: 404.php');
+}
+$query_check_koppeling = "SELECT * FROM stempelkaarten WHERE ondernemer_id ='".$ondernemer_id."' AND stempelkaart_id ='".$kaart_id."';";
+$result_check_koppeling = mysqli_query($con, $query_check_koppeling);
+if(mysqli_num_rows($result_check_koppeling) == 0){
+    header('Location: 404.php');
+}
+
+
+
 
 ?>
 <!DOCTYPE html>
@@ -38,8 +70,9 @@ include("config.php")
     ?>
 </nav>
 <div class="wrapperQRinfo">
-        <h1>QR-Code Informatie</h1>
-        <h4>Naam</h4> <p>Jelmer de Jong</p>
+    <form action="ondernemer_qrcode_gescand.php?kaartid=<?echo$kaart_id?>&klantid=<?echo$klant_id?>">
+        <h1>QR-Code Informati</h1>
+        <h4>Naam</h4> <p><?echo$klant_naam?></p>
         <h4>Aantal stempels</h4> <p>4 / 12</p>
         <h4>Stempels Toevoegen</h4> <input type="number" value="1" min="1" max="12"> <!--max moet resterend aantal stempels zijn!-->
         <div style="border-bottom: 1px solid #dee0e4"></div>
@@ -54,6 +87,7 @@ include("config.php")
             window.history.back();
         }
     </script>
+    </form>
 </div>
 
 </body>
