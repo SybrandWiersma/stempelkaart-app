a360b4d1df0de1fea3a80df33119b981b67f365f
Sybrand Wiersma <sybrandw@live.nl>
2020-04-14 21:03:46 +0200

    save4

--- ondernemer_qrcode_gescand.php
+++ ondernemer_qrcode_gescand.php
@@ -77,15 +77,24 @@ $row_aantstemps = mysqli_fetch_array($result_aantstemps);
     ?>
 </nav>
 <div class="wrapperQRinfo">
+    <h1>QR-Code Informatie</h1>
+
+    <h4>Naam</h4> <p><?php echo $row_klantdata['naam_klant']?></p>
+    <h4>Aantal stempels</h4> <p><?php echo $row_aantstemps['aant_stemps']." / ".$row_kaartdata['beloning_aantstemps']?></p>
+
     <form action="ondernemer_qrcode_gescand.php?kaartid=<?php echo$kaart_id?>&klantid=<?php echo$klant_id?>" method="post">
-        <h1>QR-Code Informatie</h1>
-        <h4>Naam</h4> <p><?php echo $row_klantdata['naam_klant']?></p>
-        <h4>Aantal stempels</h4> <p><?php echo $row_aantstemps['aant_stemps']." / ".$row_kaartdata['beloning_aantstemps']?></p>
-        <h4>Stempels Toevoegen</h4> <input type="number" id="stempel_waarde" value="1" min="1" max="<?php echo $row_kaartdata['beloning_aantstemps']?>"> <!--max moet resterend aantal stempels zijn!-->
-        <div style="border-bottom: 1px solid #dee0e4"></div>
-    <button <?php if ($row_aantstemps['aant_stemps'] == $row_kaartdata['beloning_aantstemps'])echo "style =\"background-color:#4d5563; opacity:50%\" disabled"?> id="test" type="submit" value="test">Stempel(s) Zetten</button>
+
+    <h4>Stempels Toevoegen</h4> <input type="number" name="stempel_aantal" value="1" min="1" max="<?php echo $row_kaartdata['beloning_aantstemps'] - 1 ?>"> <!--max moet resterend aantal stempels zijn!-->
+    <div style="border-bottom: 1px solid #dee0e4"></div>
+    <button <?php if ($row_aantstemps['aant_stemps'] == $row_kaartdata['beloning_aantstemps'])echo "style =\"background-color:#4d5563; opacity:50%\" disabled"?> name="stempelzetten" type="submit" value="1">Stempel(s) Zetten</button>
+    </form>
+    <?php if (isset($_POST['stempelzetten']) && isset($_POST['stempel_aantal'])) {
+        stml =
+    }
+
+
     <button <?php if($row_aantstemps['aant_stemps'] != $row_kaartdata['beloning_aantstemps']) echo "style =\"background-color:#4d5563; opacity:50%\" disabled";?>>Kaart Verzilveren</button>
-    <button onclick="window.location.href'#'">Kaart Wijzigen</button>
+    <button name="kaartwijzigen" value="2">Kaart Wijzigen</button>
     <button style="background-color: red; margin-bottom: 5%">Kaart Verwijderen</button>
     <div style="border-bottom: 1px solid #dee0e4"></div>
     <button onclick="goBack()" style="width: 40%; margin-bottom: 5%"><i class="fas fa-chevron-left"></i> Terug</button> <br>
@@ -94,7 +103,6 @@ $row_aantstemps = mysqli_fetch_array($result_aantstemps);
             window.history.back();
         }
     </script>
-    </form>
 </div>
 
 </body>
