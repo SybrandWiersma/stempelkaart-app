9be15fc911c8dc4261177aec51303269b1d209df
Unknown <frederic.de.haan@student.nhlstenden.com>
2020-04-27 15:53:12 +0200

    header
    klant-, index- en inlogheader gemaakt en included

---
+++ header_login.php
@@ -0,0 +1,38 @@
+<?php
+if (isset($_SESSION['gebruikersnaam'])) {
+    session_destroy();
+    header('Location: index.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>StempelkaartApp</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+</head>
+<body>
+
+<nav class="navtop">
+    <?php
+    if (!isset($_SESSION['klant'])) {
+
+        ?>
+        <div>
+            <h1><a href="index.php">StempelkaartApp</a></h1>
+            <a href="ondernemer_registeren.php"><i class="fas fa-user-circle"></i>Registreren als ondernemer</a>
+            <a href="loginpagina.php"><i class="fas fa-sign-out-alt"></i>Inloggen</a>
+        </div>
+        <?php
+    } else {
+        ?>
+        <div>
+            <h1><a href="">StempelkaartApp</a></h1>
+            <a href="klant_gegevens.php"><i class="fas fa-user-circle"></i>Profiel</a>
+            <a href="klant_stempelkaartoverzicht.php?uitloggen"><i class="fas fa-sign-out-alt"></i>Uitloggen</a>
+        </div>
+        <?php
+    }
+    ?>
+</nav>
--- klant_gegevens.php
+++ klant_gegevens.php
@@ -1,192 +1,143 @@
 <?php
 include("config.php");
+require("header_klant.php");
 
-// Check of gebruiker ingelogd is of niet
-if (!isset($_SESSION['klant'])) {
-    header('Location: index.php');
-}
-// Uitloggen (eerste check of er een 'x' in de browser meegegeven wordt, zoja als dat uitloggen is word je uitgelogd)
-if (isset($_GET['x'])) {
-    if ($_GET['x'] == "uitloggen") {
-        session_destroy();
-        header('Location: index.php');
+$error_message = "";
+$success_message = "";
+$ww_message = "";
+
+//query om klant gegevens uit de database op te halen
+$sql_id = "SELECT  * FROM `klanten` WHERE `gebr_naam`='" . $_SESSION['klant'] . "'";
+$sql_query_id = mysqli_query($con, $sql_id);
+$result_id = mysqli_fetch_object($sql_query_id);
+
+if (isset($_POST['ww'])) {
+    $wachtwoord_o = trim($_POST['wachtwoord_o']);
+    $wachtwoord_n = trim($_POST['wachtwoord_n']);
+    $wachtwoord_h = trim($_POST['wachtwoord_h']);
+
+    $test = true;
+
+    if ($wachtwoord_o == '' || $wachtwoord_n == '' || $wachtwoord_h == '') {
+        $test = false;
+        $ww_message = "Het is verplicht om alle velden in te vullen!";
+    }
+
+    // Check of oude wachtwoord klopt
+    if ($test && ($wachtwoord_o != $result_id->wachtwoord)) {
+        $test = false;
+        $ww_message = "Uw oude wachtwoord klopt niet";
+    }
+
+    // Check of oude wachtwoord klopt
+    if ($test && ($wachtwoord_o == $wachtwoord_n)) {
+        $test = false;
+        $ww_message = "Uw nieuwe wachtwoord mag niet hetzelfde zijn als uw oude wachtwoord";
+    }
+
+    // Check of de wachtwoorden exact hetzelfde zijn
+    if ($test && ($wachtwoord_n != $wachtwoord_h)) {
+        $test = false;
+        $ww_message = "Nieuwe wachtwoorden komen niet overeen";
+    }
+
+    // Check of wachtwoord te kort is
+    if ($test && strlen($wachtwoord_n) < 5) {
+        $test = false;
+        $ww_message = "Het wachtwoord moet minimaal uit vijf tekens bestaan.";
+    }
+
+    // Als alles klopt, query uitvoeren om in de database te plaatsen.
+    if ($test) {
+        $insertSQL = "UPDATE `klanten` SET `wachtwoord`='" . $wachtwoord_n . "' WHERE `gebr_naam`='" . $_SESSION['klant'] . "'";
+        $stmt = $con->prepare($insertSQL);
+        $stmt->execute();
+        $stmt->close();
+
+        $wws_message = "U heeft uw wachtwoord aangepast!</br>";
     }
 }
 
 
+// Na registreren
+if (isset($_POST['aanpassen'])) {
+
+    $gebr_naam = trim($_POST['gebruikersnaam']);
+
+    $email = trim($_POST['email']);
+    $telefoonnummer = trim($_POST['telefoonnummer']);
+
+
+    //wachtwoord encoding werkt nog niet helemaal
+    //$hash = password_hash($_POST['wachtwoord'], PASSWORD_DEFAULT, ['cost' => 12]);
+
+    $klopt = true;
+
+    // Check of alle velden ingevuld zijn
+    if ($gebr_naam == '' || $email == '' || $telefoonnummer == '') {
+        $klopt = false;
+        $error_message = "Het is verplicht om alle velden in te vullen!";
+    }
+
+
+    // Check of er een ongeldig email adres ingevuld is
+    if ($klopt && !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $klopt = false;
+        $error_message = "Vul een geldig email adres in.";
+    }
+
+    // Check of er een ongeldig Telefoonnummer ingevuld is
+    if ($klopt && !preg_match('/^[0-9]{10}+$/', $telefoonnummer)) {
+        $klopt = false;
+        $error_message = "Vul een geldig telefoonnummer in (bijvoorbeeld: 0612345678).";
+    }
+
+
+    if ($klopt) {
+
+        // Check of telefoonnummer al voorkomt in de database
+        $stmt = $con->prepare("SELECT * FROM klanten WHERE tel_nr = ?");
+        $stmt->bind_param("s", $telefoonnummer);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $stmt->close();
+        if ($telefoonnummer != $result_id->tel_nr && $result->num_rows > 0) {
+            $klopt = false;
+            $error_message = "Dit telefoonnummer is al bekend in ons systeem.</br>";
+        }
+
+    }
+
+
+    if ($klopt) {
+
+        // Check of email al voorkomt in de database
+        $stmt = $con->prepare("SELECT * FROM klanten WHERE email = ?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $stmt->close();
+
+        if ($email != $result_id->email && $result->num_rows > 0) {
+            $klopt = false;
+            $error_message = "Dit email adres is al bekend in ons systeem.</br>";
+        }
+
+    }
+
+
+    // Als alles klopt, query uitvoeren om in de database te plaatsen.
+    if ($klopt) {
+        $insertSQL = "UPDATE `klanten` SET `email`='" . $email . "', `tel_nr`='" . $telefoonnummer . "' WHERE `gebr_naam`='" . $_SESSION['klant'] . "'";
+        $stmt = $con->prepare($insertSQL);
+        $stmt->execute();
+        $stmt->close();
+
+        $success_message = "Uw account is aangepast!</br>";
+    }
+}
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Uw Gegevens</title>
-    <link rel="stylesheet" href="style.css" type="text/css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 
-    <?php
-    $error_message = "";
-    $success_message = "";
-    $ww_message = "";
-
-    //query om klant gegevens uit de database op te halen
-    $sql_id = "SELECT  * FROM `klanten` WHERE `gebr_naam`='" . $_SESSION['klant'] . "'";
-    $sql_query_id = mysqli_query($con, $sql_id);
-    $result_id = mysqli_fetch_object($sql_query_id);
-
-    if (isset($_POST['ww'])) {
-        $wachtwoord_o = trim($_POST['wachtwoord_o']);
-        $wachtwoord_n = trim($_POST['wachtwoord_n']);
-        $wachtwoord_h = trim($_POST['wachtwoord_h']);
-
-        $test = true;
-
-        if ($wachtwoord_o == '' || $wachtwoord_n == '' || $wachtwoord_h == '') {
-            $test = false;
-            $ww_message = "Het is verplicht om alle velden in te vullen!";
-        }
-
-        // Check of oude wachtwoord klopt
-        if ($test && ($wachtwoord_o != $result_id->wachtwoord)) {
-            $test = false;
-            $ww_message = "Uw oude wachtwoord klopt niet";
-        }
-
-        // Check of oude wachtwoord klopt
-        if ($test && ($wachtwoord_o == $wachtwoord_n)) {
-            $test = false;
-            $ww_message = "Uw nieuwe wachtwoord mag niet hetzelfde zijn als uw oude wachtwoord";
-        }
-
-        // Check of de wachtwoorden exact hetzelfde zijn
-        if ($test && ($wachtwoord_n != $wachtwoord_h)) {
-            $test = false;
-            $ww_message = "Nieuwe wachtwoorden komen niet overeen";
-        }
-
-        // Check of wachtwoord te kort is
-        if ($test && strlen($wachtwoord_n) < 5) {
-            $test = false;
-            $ww_message = "Het wachtwoord moet minimaal uit vijf tekens bestaan.";
-        }
-
-        // Als alles klopt, query uitvoeren om in de database te plaatsen.
-        if ($test) {
-            $insertSQL = "UPDATE `klanten` SET `wachtwoord`='" . $wachtwoord_n . "' WHERE `gebr_naam`='" . $_SESSION['klant'] . "'";
-            $stmt = $con->prepare($insertSQL);
-            $stmt->execute();
-            $stmt->close();
-
-            $wws_message = "U heeft uw wachtwoord aangepast!</br>";
-        }
-    }
-
-
-    // Na registreren
-    if (isset($_POST['aanpassen'])) {
-
-        $gebr_naam = trim($_POST['gebruikersnaam']);
-
-        $email = trim($_POST['email']);
-        $telefoonnummer = trim($_POST['telefoonnummer']);
-
-
-        //wachtwoord encoding werkt nog niet helemaal
-        //$hash = password_hash($_POST['wachtwoord'], PASSWORD_DEFAULT, ['cost' => 12]);
-
-        $klopt = true;
-
-        // Check of alle velden ingevuld zijn
-        if ($gebr_naam == '' || $email == '' || $telefoonnummer == '') {
-            $klopt = false;
-            $error_message = "Het is verplicht om alle velden in te vullen!";
-        }
-
-
-        // Check of er een ongeldig email adres ingevuld is
-        if ($klopt && !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $klopt = false;
-            $error_message = "Vul een geldig email adres in.";
-        }
-
-        // Check of er een ongeldig Telefoonnummer ingevuld is
-        if ($klopt && !preg_match('/^[0-9]{10}+$/', $telefoonnummer)) {
-            $klopt = false;
-            $error_message = "Vul een geldig telefoonnummer in (bijvoorbeeld: 0612345678).";
-        }
-
-
-        if ($klopt) {
-
-            // Check of telefoonnummer al voorkomt in de database
-            $stmt = $con->prepare("SELECT * FROM klanten WHERE tel_nr = ?");
-            $stmt->bind_param("s", $telefoonnummer);
-            $stmt->execute();
-            $result = $stmt->get_result();
-            $stmt->close();
-            if ($telefoonnummer != $result_id->tel_nr && $result->num_rows > 0) {
-                $klopt = false;
-                $error_message = "Dit telefoonnummer is al bekend in ons systeem.</br>";
-            }
-
-        }
-
-
-        if ($klopt) {
-
-            // Check of email al voorkomt in de database
-            $stmt = $con->prepare("SELECT * FROM klanten WHERE email = ?");
-            $stmt->bind_param("s", $email);
-            $stmt->execute();
-            $result = $stmt->get_result();
-            $stmt->close();
-
-            if ($email != $result_id->email && $result->num_rows > 0) {
-                $klopt = false;
-                $error_message = "Dit email adres is al bekend in ons systeem.</br>";
-            }
-
-        }
-
-
-        // Als alles klopt, query uitvoeren om in de database te plaatsen.
-        if ($klopt) {
-            $insertSQL = "UPDATE `klanten` SET `email`='" . $email . "', `tel_nr`='" . $telefoonnummer . "' WHERE `gebr_naam`='" . $_SESSION['klant'] . "'";
-            $stmt = $con->prepare($insertSQL);
-            $stmt->execute();
-            $stmt->close();
-
-            $success_message = "Uw account is aangepast!</br>";
-        }
-    }
-    ?>
-</head>
-<body>
-<nav class="navtop">
-    <?php
-    //check of gebruiker niet ingelogd is, dan weergeef je de registratie links en inlog link
-    if (!isset($_SESSION['klant'])) {
-
-        ?>
-
-        <div>
-            <h1><a href="index.php">StempelkaartApp</a></h1>
-            <a href="ondernemer_registeren.php"><i class="fas fa-user-circle"></i>Registreren als ondernemer</a>
-            <a href="klant_registratie.php"><i class="fas fa-user-circle"></i>Registreren als klant</a>
-            <a href="loginpagina.php"><i class="fas fa-sign-out-alt"></i>Inloggen</a>
-        </div>
-        <?php
-        //wanneer gebruiker wel ingelogd is weergeef je de links naar profiel en uitlog knop
-    } else {
-        ?>
-        <div>
-            <h1><a href="klant_stempelkaartoverzicht.php">StempelkaartApp</a></h1>
-            <a href="klant_gegevens.php"><i class="fas fa-user-circle"></i>Profiel</a>
-            <a href="klant_gegevens.php?x=uitloggen"><i class="fas fa-sign-out-alt"></i>Uitloggen</a>
-        </div>
-        <?php
-    }
-    ?>
-</nav>
 <div class="wrapper">
     <h1>Gegevens bekijken/aanpassen</h1>
     <form action="" method="post">
